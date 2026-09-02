2a2dfe4751b26714f65ca2e9d8e262601d21f6b3
ptcpkl <ptcpkls.say.berk@gmail.com>
2026-09-02 15:40:36 +0700

    refactor: use public navigation on about page

---
+++ src/app/(blank-layout-pages)/about/layout.tsx
@@ -0,0 +1,7 @@
+import type { ChildrenType } from '@core/types'
+
+import PublicSiteLayout from '@/components/public/PublicSiteLayout'
+
+const AboutLayout = ({ children }: ChildrenType) => <PublicSiteLayout>{children}</PublicSiteLayout>
+
+export default AboutLayout
